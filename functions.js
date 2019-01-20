618fe558449f7dc0c3d1a944b976cbdda5df0622
Brian Azizi <briana@theodo.co.uk>
2019-01-20 18:04:08 +0000

    Filter out unpublished articles and images with missing src tag

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
-
 const cheerio = require('cheerio');
 const { get, findIndex } = require('lodash');
 const chalk = require('chalk');
 const moment = require('moment');
 const TurndownService = require('turndown');
-const writing = require('./writing')
+const writing = require('./writing');
+
 const { log } = console;
 const { yellow: progress } = chalk;
 
@@ -68,6 +68,7 @@ const parseImages = value => {
   const content = cheerio.load(value);
   const imagesElements = content('img');
   const images = imagesElements
+    .filter((index, img) => !!img.attribs.src)
     .map((index, { attribs: { src: imageURL, ...rest } }) => ({
       fileName: imageURL.substring(imageURL.lastIndexOf('/') + 1),
       url: imageURL,
@@ -79,68 +80,70 @@ const parseImages = value => {
 
 const dataWrangle = async (data, destination) => {
   // Iterate in every Post
-  data.rss.channel[0].item.map((post, index) => {
-    log(progress(`Currently Parsing Post No: ${index + 1}`));
+  data.rss.channel[0].item
+    .filter(post => !!post.category)
+    .filter(post => get(post, `['wp:status'][0]`) === 'publish')
+    .map((post, index) => {
+      log(progress(`Currently Parsing Post No: ${index + 1}`));
 
-    const getMeta = (key, defaultMeta = undefined) => {
-      const metaIndex = findIndex(
-        post['wp:postmeta'],
-        meta => meta['wp:meta_key'][0] === key,
-      );
-      return metaIndex !== -1
-        ? get(post, `['wp:postmeta'][${metaIndex}]['wp:meta_value'][0]`)
-        : defaultMeta;
-    };
+      const getMeta = (key, defaultMeta = undefined) => {
+        const metaIndex = findIndex(
+          post['wp:postmeta'],
+          meta => meta['wp:meta_key'][0] === key,
+        );
+        return metaIndex !== -1
+          ? get(post, `['wp:postmeta'][${metaIndex}]['wp:meta_value'][0]`)
+          : defaultMeta;
+      };
 
-    let content = post['content:encoded'][0];
-    let images = parseImages(content);
-    images.forEach(image => {
-      content = content.replace(
-        new RegExp(image.url, 'g'),
-        `./${image.fileName}`,
-      );
+      let content = post['content:encoded'][0];
+      let images = parseImages(content);
+      images.forEach(image => {
+        content = content.replace(
+          new RegExp(image.url, 'g'),
+          `./${image.fileName}`,
+        );
+      });
+
+      const thumbnail = getMeta('essb_cached_image');
+      images =
+        thumbnail !== undefined
+          ? [
+              {
+                url: thumbnail,
+                fileName: thumbnail.substring(thumbnail.lastIndexOf('/') + 1),
+              },
+              ...images,
+            ]
+          : images;
+
+      content = turndownService.turndown(content);
+
+      const header = {
+        title: `"${get(post, 'title[0]')}"`,
+        image: thumbnail
+          ? `./${thumbnail.substring(thumbnail.lastIndexOf('/') + 1)}`
+          : undefined,
+        author: get(post, `['dc:creator'][0]`),
+        date: moment(get(post, 'pubDate[0]')).format(),
+        categories: `[${post.category.map(
+          (category, categoriesIndex) =>
+            `${categoriesIndex > 0 ? ' ' : ''}"${category._}"`,
+        )}]`,
+        slug: get(post, `['wp:post_name'][0]`) || undefined,
+        excerpt: get(post, `['excerpt:encoded'][0]`)
+          ? `"${get(post, `['excerpt:encoded'][0]`)}"`
+          : undefined,
+        draft: get(post, `['wp:status'][0]`) !== 'publish',
+        meta_title: `"${getMeta('_yoast_wpseo_title', get(post, 'title[0]'))}"`,
+        twitter_shares: getMeta('essb_c_twitter'),
+        facebook_shares: getMeta('essb_c_facebook'),
+        kksr_ratings: getMeta('_kksr_ratings'),
+        kksr_casts: getMeta('_kksr_casts'),
+      };
+
+      return writing(header, images, content, destination);
     });
-
-    const thumbnail = getMeta('essb_cached_image');
-    images =
-      thumbnail !== undefined
-        ? [
-            {
-              url: thumbnail,
-              fileName: thumbnail.substring(thumbnail.lastIndexOf('/') + 1),
-            },
-            ...images,
-          ]
-        : images;
-
-    content = turndownService.turndown(content);
-
-    const header = {
-      layout: 'post',
-      title: `"${get(post, 'title[0]')}"`,
-      image: thumbnail
-        ? `./${thumbnail.substring(thumbnail.lastIndexOf('/') + 1)}`
-        : undefined,
-      author: get(post, `['dc:creator'][0]`),
-      date: moment(get(post, 'pubDate[0]')).format(),
-      categories: `[${post.category.map(
-        (category, categoriesIndex) =>
-          `${categoriesIndex > 0 ? ' ' : ''}"${category._}"`,
-      )}]`,
-      slug: get(post, `['wp:post_name'][0]`) || undefined,
-      excerpt: get(post, `['excerpt:encoded'][0]`)
-        ? `"${get(post, `['excerpt:encoded'][0]`)}"`
-        : undefined,
-      draft: get(post, `['wp:status'][0]`) !== 'publish',
-      meta_title: `"${getMeta('_yoast_wpseo_title', get(post, 'title[0]'))}"`,
-      twitter_shares: getMeta('essb_c_twitter'),
-      facebook_shares: getMeta('essb_c_facebook'),
-      kksr_ratings: getMeta('_kksr_ratings'),
-      kksr_casts: getMeta('_kksr_casts'),
-    };
-
-    return writing(header, images, content, destination);
-  });
 };
 
-module.exports = { dataWrangle: dataWrangle };
+module.exports = { dataWrangle };
